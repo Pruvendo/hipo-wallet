84db0c030f01c0619fc17e0bb456e6b4a8c93d8e
Ivan <ivan@my3dshoes.com>
2024-05-26 09:45:33 +0300

    to tokens_minted

--- src/HipoWallet.v
+++ src/HipoWallet.v
@@ -34,7 +34,10 @@ UseLocal Definition _ := [ TvmBuilder ;
                            optional TvmBuilder; 
                            address ; 
                            int256 ;
+                           uint256 ;
                           int64 ;
+                          uint64 ;
+                          bool;
                            TvmCell ; 
                            TvmSlice ; 
                            (optional (TvmCell ** TvmSlice));
@@ -262,8 +265,183 @@ Ursus Definition send_tokens(src:slice_)(s:slice_)(fwd_fee:uint256):UExpression 
         (* .store_slice(forward_payload); *)
         ::// b -> store (forward_payload) .
      (* send_msg(true, wallet, state_init, receive, 0, send::remaining_value); *)
-
+    refine __return__. 
 }
+return.
+Defined.
+Sync.
+
+Ursus Definition get_storage_fee : UExpression int256 false.
+{
+    refine __return__.
+}
+return \\ tokens.
+Defined.
+Sync.
+
+Ursus Definition wallet_storage_fee:UExpression int256 false.
+{
+    ::// var0 cells:uint256:= {1} + {1};_|.
+    ::// var0 bits:uint256:= {267} + {267} + {124};_|. 
+    ::// var0 duration:uint256:= {60} * {60} * {24} * {365} * {5}; _|.
+    refine __return__.
+}
+return  \\ tokens(* get_storage_fee(cells, bits, duration, false) *).
+Defined.
+Sync.
+
+Ursus Definition raw_reserve(amount:int256)(mode:int256) : UExpression PhantomType false.
+{
+    refine __return__.
+}
+return.
+Defined.
+Sync.
+
+
+
+
+Ursus Definition receive_tokens(src:slice_)(s:slice_):UExpression PhantomType true.
+{
+    (* int query_id = s~load_uint(64); *)
+    ::// var0 query_id:_:=s -> load(int256);_|. (* ????????????????????? *)
+
+    (* int amount = s~load_coins(); *)
+    ::// var0 amount:_:=s -> load(int256);_|.
+
+    (* slice sender = s~load_msg_addr(); *)
+    ::// var0 sender_address : address := s -> load (address) ; _ | .
+    ::// var0 sender_builder : TvmBuilder; _ |.
+    ::// sender_builder -> store (sender_address) ; _ |.
+    ::// var0 sender:_:= sender_builder -> toCell() -> toSlice () ; _ | .
+
+    (* slice return_excess = s~load_msg_addr(); *)
+    ::// var0 return_address : address := s -> load (address) ; _ | .
+    ::// var0 return_builder : TvmBuilder; _ |.
+    ::// return_builder -> store (return_address) ; _ |.
+    ::// var0 return_excess:_:= return_builder -> toCell() -> toSlice () ; _ | .
+
+    (* int forward_ton_amount = s~load_coins(); *)
+    ::// var0 forward_ton_amount:_:=s -> load(int256);_|.
+
+    ::// var0 forward_payload:_ := s;_|.
+
+    (* ( _, _, int wallet_addr ) = create_wallet_address(sender.to_builder(), parent, my_code()); *)
+    (* ( int src_wc, int src_addr ) = parse_std_addr(src); *)
+
+    (* throw_unless(err::access_denied, (src_wc == chain::base) & (src_addr == wallet_addr)); *)
+    ::// require_ (((* src_wc *) {true} == {true} (* chain::base *)) 
+                       && ((* src_addr *) {true} == {true} (* wallet_addr *)) (* , err::access_denied *) ) ;_ | .
+
+    ::// tokens += amount.
+
+    ::// if(forward_ton_amount) then { ->/> }.
+       {
+        (* builder notification = begin_cell() *)
+         ::// var0 notification : TvmBuilder ; _ |.
+        (* .store_uint(op::transfer_notification, 32) *)
+         ::// notification -> store ({}(* op::receive_tokens, 32 *)) .
+        (* .store_uint(query_id, 64) *)
+         ::// notification -> store (query_id(* , 64 *)) .
+            (* .store_slice(sender) *)
+        ::// notification -> store (sender) .
+            (* .store_slice(forward_payload); *)
+        ::// notification -> store (forward_payload) | .
+        (* send_msg(false, owner.to_builder(), null(), notification, forward_ton_amount,
+            send::pay_gas_separately + send::bounce_if_failed ); *)
+       }
+
+    ::// raw_reserve(wallet_storage_fee ( ), {} (* reserve::at_most *)).
+
+    (* builder excess = begin_cell() *)
+         ::// var0 excess : TvmBuilder ; _ |.
+        (* .store_uint(op::gas_excess, 32) *)
+         ::// excess -> store ({} (* op::gas_excess, 32 *)) .
+        (* .store_uint(query_id, 64); *)
+         ::// excess -> store (query_id(* , 64 *)) .
+(*     send_msg(false, return_excess.to_builder(), null(), excess, 0, send::unreserved_balance + send::ignore_errors); *)
+
+    refine __return__. 
+}
+return.
+Defined.
+Sync.
+
+Ursus Definition udict_set_builder(x:int256)(round_since:int256)(y:TvmBuilder):UExpression PhantomType true.
+{
+    ::// require_ ({true});_|.
+    refine __return__.
+}
+return.
+Defined.
+Sync.
+
+Ursus Definition tokens_minted(src:slice_)(s:slice_):UExpression PhantomType true.
+{
+    (* int query_id = s~load_uint(64); *)
+    ::// var0 query_id:_:=s -> load(int256);_|. (* ????????????????????? *)
+   (*  int amount = s~load_coins(); *)
+    ::// var0 amount:_:=s -> load(int256);_|.
+    (* int coins = s~load_coins(); *)
+    ::// var0 coins:_:=s -> load(int256);_|.
+    (* s~load_msg_addr(); ;; skip owner address *) 
+    ::// var0 xxx : address := s -> load (address) ; _ | .
+    ::// var0 yyy : TvmBuilder; _ |.
+    ::// yyy -> store (xxx) ; _ |.
+    ::// var0 zzz :_:= yyy -> toCell() -> toSlice () ; _ | .
+
+    (* int round_since = s~load_uint(32); *)
+    ::// var0 round_since:_:=s -> load(int256);_|. (* ????????????????????? *)
+    (* s.end_parse(); *)
+
+    (* throw_unless(err::access_denied, equal_slice_bits(src, parent)); *)
+    ::// require_ (equal_slice_bits(src, parent) (*, err::access_denied *) ) ;_|.
+
+    ::// tokens += amount .
+
+    ::// if(round_since) then { ->/> }.
+    {
+       (*  ( slice v, _ ) = staking~udict_delete_get?(32, round_since); *)
+       (*  int staking_coins = v~load_coins(); *)
+       ::// var0 staking_coins:_:=s -> load(int256);_|.
+        (* v.end_parse(); *)
+        ::// staking_coins -= coins.
+        ::// if(staking_coins) then { ->/> } | .
+        {
+            ::// var0 qqq : TvmBuilder ; _ |.
+            (* staking~udict_set_builder(32, round_since, begin_cell().store_coins(staking_coins)); *)
+           ::// (* staking~ *) udict_set_builder({}, {} (* round_since *) , {} (* qqq -> store(staking_coins)*)) | .
+        }
+    }
+
+    (* raw_reserve(wallet_storage_fee(), reserve::at_most); *)
+    ::// raw_reserve(wallet_storage_fee ( ), {} (* reserve::at_most *)).
+
+   (*  builder notification = begin_cell() *)
+         ::// var0 notification : TvmBuilder ; _ |.
+       (*  .store_uint(op::transfer_notification, 32) *)
+         ::// notification -> store ({}(* op::transfer_notification, 32 *)) .
+        (* .store_uint(query_id, 64) *)
+         ::// notification -> store (query_id(* 64 *)) .
+        (* .store_coins(amount) *)
+        ::// notification -> store (amount) .
+        (* .store_slice(owner) *)
+        ::// notification -> store (owner) .
+        (* .store_int(false, 1); *)
+        ::// notification -> store ({false} (*  *)) . (* ????????????????????? *)
+    refine __return__.
+}
+return.
+Defined.
+Sync.
+
+
+
+
+
+
+
+
 
 
 
