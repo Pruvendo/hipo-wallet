f6390e3fa432a0e6f8b08ccf3f2643e8b92beee1
Evgeny Kosogorov <evgeny.kosogorov@technavology.com>
2024-06-19 20:24:13 +0400

    MTO.3

--- src/CommonQC.v
+++ src/CommonQC.v
@@ -54,6 +54,52 @@ Instance Ledger_Shrink : Shrink (LedgerLRecord rec) := {
     shrink l := Datatypes.nil : Datatypes.list (LedgerLRecord rec)
 }.
 
+Definition max_key_with_default
+    {K V : Type}
+    `{XDefault K}
+    `{XOrdered bool K}
+    (map : mapping K V)
+    : K :=
+        xMaybeMapDefault (fun x => x) (xHMapMaxKey map) default.
+
+Definition map_next_key_gen
+    {n : N}
+    {V : Type}
+    (map : mapping (XUBInteger n) V)
+    (size : nat)
+    : G (XUBInteger n) :=
+        let max_key := max_key_with_default map in
+            d <- (arbitrarySized size : G (XUBInteger n)) ;;
+            ret (xIntPlus max_key (xIntPlus d xInt1)).
+
+Definition mapping_gen_sized 
+  {K V : Type}
+  (gk : G K)
+  (gv : G V)
+  (eq_dec : forall k1 k2 : K, {k1 = k2} + {k1 <> k2})
+  (size : nat)
+  : G (mapping K V) :=
+    mapping_size <- ((arbitrarySized size) : G nat) ;;
+    keys <- vectorOf mapping_size gk ;;
+    values <- vectorOf mapping_size gv ;;
+    let keys' := nodup eq_dec keys in
+      @ret G _ _ (xHMapFromList (combine keys' values)) : G (mapping K V).
+    
+Definition non_empty_map_gen
+  {n : N}
+  {V : Type}
+  (gv : G V)
+  (size : nat)
+  : G (mapping (XUBInteger n) V) := 
+    element <- gv ;;
+    map <- ((mapping_gen_sized (arbitrarySized size) gv uint_eq_dec size) : G (mapping (XUBInteger n) V)) ;;
+    id <- (map_next_key_gen map size : G (XUBInteger n)) ;;
+    ret
+      (let (ids, elements) := split (xHMapToList map) in
+        xHMapFromList (combine 
+                      (nodup uint_eq_dec (id :: ids)) 
+                      (element :: elements))).
+
 Notation " f '::>' g" := 
     ((_ : EmbeddedType f (@field_type f _ _ g))) 
     (at level 20, only parsing, left associativity).
@@ -72,6 +118,7 @@ Definition Parent := Contract :> _parent.
 Definition Owner := Contract :> _owner.
 Definition Unstaking := Contract :> _unstaking.
 Definition Tokens := Contract :> _tokens.
+Definition Staking := Contract :> _staking.
 
 Definition with_positive_int
     {n : N}
@@ -81,28 +128,58 @@ Definition with_positive_int
         let positive := if (xIntGeb v 0%Z) then v else (xIntMult ((-1)%Z : XBInteger n) v) in
         ret (l <-- positive with et).
 
+Definition store_builder_ {A : Type} `{SolTypable A} (a : A) (b : TvmBuilder) := builder_store_ b a.
+
+Definition pack_builder (f : TvmBuilder -> option TvmBuilder) : TvmBuilder :=
+    xMaybeMapDefault id (f (Wrap_TvmBuilder EmptyPrecell)) default.
+
 Definition pack_slice (f : TvmBuilder -> option TvmBuilder) : TvmSlice :=
     xMaybeMapDefault
         builder_to_slice_
         (f (Wrap_TvmBuilder EmptyPrecell))
         default.
 
-Definition pack_address (addr : address) : TvmSlice := pack_slice (fun x => builder_store_ x addr).
+Definition pack_cell (f : TvmBuilder -> option TvmBuilder) : TvmCell :=
+    xMaybeMapDefault
+        builder_to_cell_
+        (f (Wrap_TvmBuilder EmptyPrecell))
+        default.
+
+Definition pack_slice_address (addr : address) : TvmSlice := pack_slice (store_builder_ addr).
 
 Definition with_slice_address_sized
     (et : EmbeddedType (LedgerLRecord rec) TvmSlice)
     (size : nat)
     (l : LedgerLRecord rec) : G (LedgerLRecord rec) :=
         addr <- (arbitrarySized size : G address) ;;
-        let slice_address := pack_address addr in
+        let slice_address := pack_slice_address addr in
         ret (l <-- slice_address with et).
+
+Definition with_bool
+    (et : EmbeddedType (LedgerLRecord rec) bool)
+    (l : LedgerLRecord rec) : G (LedgerLRecord rec) :=
+        ret (l <-- false with et).
+
+Definition cell_uint256_gen_sized (size : nat) : G (TvmCell) :=
+    v <- (arbitrarySized size : G uint256) ;;
+    ret (pack_cell (store_builder_ v)). 
+
+Definition mapping_uint32_cell_gen_sized (size : nat) : G (mapping uint32 TvmCell) :=
+    non_empty_map_gen (cell_uint256_gen_sized size) size.
+
+Definition with_staking (size : nat) (l : LedgerLRecord rec) : G (LedgerLRecord rec) :=
+    m <- mapping_uint32_cell_gen_sized size ;;
+    ret (l <-- (pack_cell (store_builder_ m)) with Staking).
 
 Definition ledger_gen_sized (size : nat) : G (LedgerLRecord rec) :=
     arbitrarySized size
         >>= with_positive_int Unstaking
         >>= with_positive_int Tokens
         >>= with_slice_address_sized Parent size
-        >>= with_slice_address_sized Owner size.
+        >>= with_slice_address_sized Owner size
+        >>= with_staking size.
+
+Sample (with_staking 0 default).
 
 #[global]
 Instance Ledger_GenSized : GenSized (LedgerLRecord rec) := {
--- src/Props/Props.v
+++ src/Props/Props.v
@@ -2,8 +2,35 @@ Require Import UrsusEnvironment.Solidity.current.Environment.
 Require Import Common.
 Require Import CommonQC.
 Require Import UrsusQC.ConvertPropToChecker.
+Require Import UrsusQC.CreateDiagnostics.
 Require Import HipoWallet.
 Import HipoWallet.
+
+Require Import ExtLib.Data.Monads.OptionMonad.
+Require Import ExtLib.Data.Option.
+Require Import ExtLib.Structures.Monad.
+Import QuickChick.
+
+Import MonadNotation.
+Open Scope monad_scope.
+
+Definition slice_decode_with_default (A : Type) `{SolTypable A} `{XDefault A} (slice : TvmSlice) : A :=
+    xMaybeMapDefault fst (slice_decode_ A slice) default.
+
+Definition cell_decode_with_default (A : Type) `{SolTypable A} `{XDefault A} (cell : TvmCell) : A :=
+    xMaybeMapDefault fst (slice_decode_ A (cell_to_slice_ cell)) default.
+
+Definition cr_get_or_default {b : bool} {A : Type} `{XDefault A} (cr : ExecGenDefs.ControlResult A b) : A.
+destruct cr; [exact a | | |].
+all: exact(default).
+Defined.
+
+Definition uint32_to_int256 (n : uint32) : int256.
+destruct n.
+constructor.
+exact (Z.of_N n).
+Defined.
+
 
 Definition pack_slice_save_coins (query_id: int64)
 (coins :int256)
@@ -18,11 +45,17 @@ Definition pack_slice_save_coins (query_id: int64)
                 (pack_sized 32 (round_since : int32)))).
 
 Definition pack_slice_tokens_minted
-(query_id: int64)
+(query_id: uint64)
 (amount :int256)
-(coins :int256)
-(addr :int256) (* address*)
-(round_since : int32) : TvmSlice :=
+(coins :uint256)
+(addr :address)
+(round_since : uint32) : TvmSlice :=
+    pack_slice
+        (store_builder_ query_id >=>
+        store_builder_ amount >=>
+        store_builder_ coins >=>
+        store_builder_ addr >=>
+        store_builder_ round_since).
 
 (* (xMaybeMapDefault builder_to_slice_ (
         
@@ -30,19 +63,6 @@ builder_store_  (
         xMaybeMapDefault  Datatypes.id  (builder_store_  (Wrap_TvmBuilder EmptyPrecell) query_id) default) 
                  amount  ) default).
  *)   
-        (Wrap_TvmSlice
-            (precell_push
-                    (precell_push
-                    (precell_push
-                    (precell_push
-                    (precell_push 
-                        EmptyPrecell 
-                        (pack_sized 64 (query_id : int64)))
-                    (pack_sized 256 (amount : int256)))
-                    (pack_sized 256 (coins : int256)))
-                    (pack_sized 256 (addr : int256))) (*address*)
-                (pack_sized 32 (round_since : int32)))).
-
 
 Definition pack_slice_unstake_tokens (query_id: int64)
 (amount :int256)
@@ -123,22 +143,56 @@ Definition UTO4_def
 Defined.
 
 Definition MTO3_def
- (src:TvmSlice)(query_id: int64)
+ (query_id: uint64)
  (amount :int256)
- (coins :int256)
- (addr :int256) (* address*)
- (round_since : int32)
- (l: LedgerLRecord rec): Prop.
+ (coins :uint256)
+ (addr :address)
+ (round_since : uint32)
+ (round_since_value : uint256)
+ (l0: LedgerLRecord rec): Prop.
+ elpi read_fields(l0) "staking".
+ set (staking' := 
+        cr_get_or_default (eval_state 
+            (Uinterpreter 
+                (udict_set_builder rec def 
+                    staking 
+                    32%Z 
+                    (uint32_to_int256 round_since) 
+                    (pack_builder (store_builder_ round_since_value)))) l0)).
+ set (l := l0 <-- staking' with Staking).
+ (* old version *)
+ (* set (staking := cell_decode_with_default (mapping uint32 TvmCell) (--> l0 with Staking)).
+ set (staking_value := pack_cell (store_builder_ round_since_value)).
+ set (staking' := pack_cell (store_builder_ (staking[round_since] ← staking_value))).
+ set (l := l0 <-- staking' with Staking). *)
  set (s:= pack_slice_tokens_minted query_id amount coins addr round_since).
- add_computation (l) (tokens_minted rec def src s).
+ elpi read_fields(l) "parent".
+ add_computation (l) (tokens_minted rec def parent s).
  elpi read_fields(l) "tokens".
  elpi read_fields(l') "tokens".
  condition (xIntGeb tokens 0%Z = true).
- condition (ExecGenDefs.isError (eval_state (Uinterpreter (tokens_minted rec def src s)) l) = false).
+ condition (ExecGenDefs.isError (eval_state (Uinterpreter (tokens_minted rec def parent s)) l) = false).
  conclusion (tokens' = xIntPlus tokens amount).
 Defined.
 
+Close Scope xprod_scope.
 
+(* works *)
+(* ConvertPropToChecker MTO3_def.
+QuickCheck MTO3_def_QC. *)
+
+(* Definition show_result {b : bool} {A : Type} `{Show A} (cr : ExecGenDefs.ControlResult A b) : string.
+destruct cr.
+{ exact ("value" ++ show a). }
+{ exact "break". }
+{ exact "return". }
+exact ("error" ++ show e).
+Defined.
+
+#[global]
+Instance ControlResult_Show {b : bool} {A : Type} `{Show A} : Show (ExecGenDefs.ControlResult A b) := {
+    show cr := show_result cr
+}. *)
 
 (*
 Definition SCO2_def
